5d589e3f2a924a6b4d94343d1655fba54ea8f495
Abdelali-Moutawassit2 <abdelalimoutawassit7@gmail.com>
2025-03-28 10:59:10 +0000

    commit

---
+++ utils/constants.dart
@@ -0,0 +1,50 @@
+class Constants {
+
+  // assets/App8 images
+  static const logo = 'assets/App8/images/app_icon.png';
+
+  static const homeIcon = 'assets/App8/vectors/home.svg';
+  static const categoryIcon = 'assets/App8/vectors/category.svg';
+  static const cartIcon = 'assets/App8/vectors/cart.svg';
+  static const calendarIcon = 'assets/App8/vectors/calendar.svg';
+  static const userIcon = 'assets/App8/vectors/user.svg';
+
+  static const backArrowIcon = 'assets/App8/vectors/back_arrow.svg';
+  static const addIcon = 'assets/App8/vectors/add.svg';
+  static const removeIcon = 'assets/App8/vectors/remove.svg';
+  static const searchIcon = 'assets/App8/vectors/search.svg';
+  static const themeIcon = 'assets/App8/vectors/theme.svg';
+  static const basketIcon = 'assets/App8/vectors/basket.svg';
+  static const cornIcon = 'assets/App8/vectors/corn.svg';
+  static const fireIcon = 'assets/App8/vectors/fire.svg';
+  static const emojiIcon = 'assets/App8/vectors/emoji.svg';
+  static const container = 'assets/App8/vectors/container.svg';
+
+  static const apple = 'assets/App8/vectors/apple.svg';
+  static const broccoli = 'assets/App8/vectors/broccoli.svg';
+  static const cheese = 'assets/App8/vectors/cheese.svg';
+  static const meat = 'assets/App8/vectors/meat.svg';
+
+  static const background = 'assets/App8/images/background.png';
+  static const backgroundDark = 'assets/App8/images/background_dark.png';
+  static const avatar = 'assets/App8/images/avatar.png';
+  static const calendar = 'assets/App8/images/calendar.png';
+  static const lotus = 'assets/App8/images/lotus.png';
+  static const matches = 'assets/App8/images/matches.png';
+  static const favourites = 'assets/App8/images/favourites.png';
+
+  static const lambMeat = 'assets/App8/images/lamb_meat.png';
+  static const bellPepper = 'assets/App8/images/bell_pepper.png';
+  static const cabbage = 'assets/App8/images/cabbage.png';
+  static const carrot = 'assets/App8/images/carrot.png';
+  static const cauliflower = 'assets/App8/images/cauliflower.png';
+  static const ginger = 'assets/App8/images/ginger.png';
+  static const pumpkin = 'assets/App8/images/pumpkin.png';
+  static const spinach = 'assets/App8/images/spinach.png';
+  static const tomatoes = 'assets/App8/images/tomatoes.png';
+
+  static const card1 = 'assets/App8/images/card1.png';
+  static const card2 = 'assets/App8/images/card2.png';
+  static const card3 = 'assets/App8/images/card3.png';
+
+}
